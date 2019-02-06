ee8ec5898b49b4b77a13cc83f1869c5f0099fc98
DiselDuay <dicduay@gmail.com>
2019-02-06 09:58:07 +0200

    examples of mechanize

---
+++ agent.rb
@@ -0,0 +1,3 @@
+require 'rubygems'
+require 'mechanize'
+pp Mechanize::AGENT_ALIASES
--- task1.rb
+++ task1.rb
@@ -5,15 +5,16 @@ require 'mechanize'
 bank_url = "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK"
 
 agent = Mechanize.new
+agent.user_agent_alias = 'Windows Chrome'
 
 page = agent.get(bank_url)
 
-form = page.form_with(:id => 'form-signin')
+form = page.form_with :id => "form-signin"
 
-form.field_with(:id => "username").value = "Pib123"
-form.field_with(:id => "password").value = "Pib123"
+form.field_with(:name => "username").value = "Pib123"
+form.field_with(:name => "password").value = "Pib123"
 
-login = agent.submit(form)
-
+login = agent.submit
+puts login.title
     
 puts "end"

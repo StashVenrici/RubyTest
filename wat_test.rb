5af4f889d1c8c95c6783d4bd070e67b3c16844e6
Stash Venrici <newstash2012@gmail.com>
2019-02-08 01:49:12 +0200

    watir task 1 testing

--- wat_test.rb
+++ wat_test.rb
@@ -1,7 +1,10 @@
 require "watir"
 
-browser = Watir::Browser.new :chrome
-browser.goto "http://www.mail.ru"
+#browser = Watir::Browser.new :chrome
+#browser.goto "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK"
 
-
-print "end program"
+browser = Watir::Browser.start "https://my.fibank.bg/oauth2-server/login?client_id=E_BANK", :chrome
+browser.text_field(name: 'username').set 'pib123'
+browser.text_field(name: 'password').set 'pib123'
+browser.button(id: 'submitBtn').click
+puts browser.element(id: 'user--identificator').text
